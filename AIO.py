f2f17559907c4a70e3f4201be24862a088316bd5
Adam Celej <celejadam01@gmail.com>
2023-01-20 19:03:47 +0100

    Update AIO.py

--- AIO.py
+++ AIO.py
@@ -94,15 +94,15 @@ class Deck(IDeck):
 class IBet(ABC):
     @property
     @abstractmethod
-    def amount(self) -> float:
+    def amount(self) -> int:
         pass
 
     @abstractmethod
-    def increase(self, amount: float) -> None:
+    def increase(self, amount: int) -> None:
         pass
 
     @abstractmethod
-    def decrease(self, amount: float) -> None:
+    def decrease(self, amount: int) -> None:
         pass
 
 class Bet(IBet):
@@ -171,6 +171,188 @@ class Hand(IHand):
             else:
                 score += int(card.rank)
         return score
+
+
+class IPlayer(ABC):
+    @property
+    @abstractmethod
+    def hand(self) -> IHand:
+        pass
+
+    @property
+    @abstractmethod
+    def bet(self) -> IBet:
+        pass
+
+    @abstractmethod
+    def is_blackjack(self) -> bool:
+        pass
+
+    @abstractmethod
+    def win(self, amount: int) -> None:
+        pass
+
+    @abstractmethod
+    def lose(self, amount: int) -> None:
+        pass
+
+
+class Player(IPlayer):
+    def __init__(self, hand: IHand, bet: IBet):
+        self._hand = hand
+        self._bet = bet
+
+    # property that returns the IHand object representing the player's hand
+    @property
+    def hand(self) -> IHand:
+        return self._hand
+
+    # property that returns the IBet object representing the player's bet
+    @property
+    def bet(self) -> IBet:
+        return self._bet
+
+    # method increases the amount of the player's bet by the given amount
+    def win(self, amount: int) -> None:
+        self._bet.increase(amount)
+
+    # method decreases the amount of the player's bet by the given amount
+    def lose(self, amount: int) -> None:
+        self._bet.decrease(amount)
+
+
+class IDealer(ABC):
+    @property
+    @abstractmethod
+    def deck(self) -> IDeck:
+        pass
+
+    @abstractmethod
+    def is_blackjack(self) -> bool:
+        pass
+
+    @abstractmethod
+    def deal(self, players: List[IPlayer]) -> None:
+        pass
+
+    @abstractmethod
+    def get_card(self) -> ICard:
+        pass
+
+
+class Dealer(IDealer):
+    def __init__(self, deck: IDeck):
+        self._deck = deck
+
+    # property that returns the IDeck object representing the dealer's deck
+    @property
+    def deck(self) -> IDeck:
+        return self._deck
+
+    # method deals two cards to each player in the list of players passed to it
+    def deal(self, players: List[IPlayer]) -> None:
+        for player in players:
+            player.hand.add_card(self._deck.draw())
+            player.hand.add_card(self._deck.draw())
+
+    # method returns a card from the top of the deck
+    def get_card(self) -> ICard:
+        return self._deck.draw()
+
+
+class IGame(ABC):
+    @property
+    @abstractmethod
+    def player(self) -> IPlayer:
+        pass
+
+    @property
+    @abstractmethod
+    def dealer(self) -> IDealer:
+        pass
+
+    @property
+    @abstractmethod
+    def deck(self) -> IDeck:
+        pass
+
+    @abstractmethod
+    def play(self) -> None:
+        pass
+
+    @abstractmethod
+    def check_win(self) -> bool:
+        pass
+
+    @abstractmethod
+    def payout(self) -> int:
+        pass
+
+
+class Game(IGame):
+    def __init__(self, player: IPlayer, dealer: IDealer, deck: IDeck):
+        self._player = player
+        self._dealer = dealer
+        self._deck = deck
+
+    @property
+    def player(self) -> IPlayer:
+        return self._player
+
+    @property
+    def dealer(self) -> IDealer:
+        return self._dealer
+
+    @property
+    def deck(self) -> IDeck:
+        return self._deck
+
+    # Method to play the game, it contains the gameplay logic
+    def play(self):
+        # dealing cards to players
+        self._dealer.deal(self._players)
+
+        # looping through each player to check for blackjack
+        for player in self._players:
+            if player.hand.is_blackjack():
+                player.win(player.bet.amount * 1.5)
+                print(f"Player has blackjack and wins {player.bet.amount * 1.5}")
+            else:
+                print(f"Player has {player.hand.score()}")
+
+        # dealer drawing cards
+        self._dealer.get_card()
+        self._dealer.get_card()
+
+        # checking if dealer has blackjack
+        if self._dealer.hand.is_blackjack():
+            for player in self._players:
+                player.lose(player.bet.amount)
+            print("Dealer has blackjack, all players lose.")
+        else:
+            # code to continue playing the game goes here
+            pass
+
+    def is_blackjack(self):
+        if len(self._cards) != 2:
+            return False
+        if (self._cards[0].rank == "Ace" and self._cards[1].rank in ["10", "Jack", "Queen", "King"]) or \
+                (self._cards[1].rank == "Ace" and self._cards[0].rank in ["10", "Jack", "Queen", "King"]):
+            return True
+        return False
+
+    def check_win(self) -> bool:
+        """
+        Method to check if the player has won or lost
+        """
+        pass
+
+    def payout(self) -> float:
+        """
+        Method to calculate the payout for the player
+        """
+        pass
+
 
 
 base.metadata.create_all(bind=engine)
